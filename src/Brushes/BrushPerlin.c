4679b2492a203d9c188746f4f3d245934d13492a
dflat2 <116065652+dflat2@users.noreply.github.com>
2024-02-08 14:07:27 +0100

    Make @Perlin non-deterministic by using a...
    ...randomly generated permutation table.

--- src/Brushes/BrushPerlin.c
+++ src/Brushes/BrushPerlin.c
@@ -18,34 +18,7 @@ Brush BrushPerlin = {
 	.Paint = &BrushPerlin_Paint,
 }; 
 
-static const int permutation[512] = {
-	158,83,170,101,150,200,118,236,63,135,149,235,109,189,153,73,207,171,157,97,
-	188,45,245,138,110,255,8,55,222,37,196,126,111,198,168,145,187,5,22,191,125,
-	12,186,179,90,129,223,44,64,182,71,162,159,76,59,215,232,18,224,15,152,74,7,
-	89,184,249,33,108,156,229,246,214,92,16,96,75,107,176,4,116,61,124,52,66,26,
-	234,154,227,40,13,3,106,24,30,228,60,56,139,122,19,190,136,54,204,80,51,2,220,
-	104,134,86,10,144,181,238,41,14,27,50,231,20,180,46,173,251,123,212,178,62,
-	210,166,130,155,137,43,199,146,161,112,206,98,160,93,201,244,113,0,94,95,69,
-	49,48,85,253,141,23,225,143,78,100,131,205,254,6,68,84,121,239,219,217,247,194,
-	91,218,233,11,119,102,35,57,169,65,1,120,203,42,105,132,221,17,38,133,53,164,
-	250,128,34,28,183,114,163,151,202,31,209,127,185,226,237,32,167,142,213,147,29,
-	177,241,99,82,252,175,79,197,208,115,148,248,72,77,25,165,81,240,174,243,39,
-	230,193,58,140,88,216,70,87,36,242,21,211,9,103,195,67,192,117,47,172,
-
-	// Repeating for easier calculations.
-	158,83,170,101,150,200,118,236,63,135,149,235,109,189,153,73,207,171,157,97,
-	188,45,245,138,110,255,8,55,222,37,196,126,111,198,168,145,187,5,22,191,125,
-	12,186,179,90,129,223,44,64,182,71,162,159,76,59,215,232,18,224,15,152,74,7,
-	89,184,249,33,108,156,229,246,214,92,16,96,75,107,176,4,116,61,124,52,66,26,
-	234,154,227,40,13,3,106,24,30,228,60,56,139,122,19,190,136,54,204,80,51,2,220,
-	104,134,86,10,144,181,238,41,14,27,50,231,20,180,46,173,251,123,212,178,62,
-	210,166,130,155,137,43,199,146,161,112,206,98,160,93,201,244,113,0,94,95,69,
-	49,48,85,253,141,23,225,143,78,100,131,205,254,6,68,84,121,239,219,217,247,194,
-	91,218,233,11,119,102,35,57,169,65,1,120,203,42,105,132,221,17,38,133,53,164,
-	250,128,34,28,183,114,163,151,202,31,209,127,185,226,237,32,167,142,213,147,29,
-	177,241,99,82,252,175,79,197,208,115,148,248,72,77,25,165,81,240,174,243,39,
-	230,193,58,140,88,216,70,87,36,242,21,211,9,103,195,67,192,117,47,172
-};
+static int s_Permutation[512] = { 0 };
 
 static double PerlinSmoothFunction(double t) {
 	return t * t * t * (t * (6 * t - 15) + 10);
@@ -98,14 +71,14 @@ static double Perlin(double x, double y, double z) {
 	double yFadedFractionalPart = PerlinSmoothFunction(yFractionalPart);
 	double zFadedFractionalPart = PerlinSmoothFunction(zFractionalPart);
 
-    int aaaHash = permutation[permutation[permutation[xIntegralPart] + yIntegralPart] + zIntegralPart];
-    int abaHash = permutation[permutation[permutation[xIntegralPart] + yIntegralPart + 1] + zIntegralPart];
-    int aabHash = permutation[permutation[permutation[xIntegralPart] + yIntegralPart] + zIntegralPart + 1];
-    int abbHash = permutation[permutation[permutation[xIntegralPart] + yIntegralPart + 1] + zIntegralPart + 1];
-    int baaHash = permutation[permutation[permutation[xIntegralPart + 1] + yIntegralPart]+ zIntegralPart];
-    int bbaHash = permutation[permutation[permutation[xIntegralPart + 1] + yIntegralPart + 1] + zIntegralPart];
-    int babHash = permutation[permutation[permutation[xIntegralPart + 1] + yIntegralPart] + zIntegralPart + 1];
-    int bbbHash = permutation[permutation[permutation[xIntegralPart + 1] + yIntegralPart + 1] + zIntegralPart + 1];
+    int aaaHash = s_Permutation[s_Permutation[s_Permutation[xIntegralPart] + yIntegralPart] + zIntegralPart];
+    int abaHash = s_Permutation[s_Permutation[s_Permutation[xIntegralPart] + yIntegralPart + 1] + zIntegralPart];
+    int aabHash = s_Permutation[s_Permutation[s_Permutation[xIntegralPart] + yIntegralPart] + zIntegralPart + 1];
+    int abbHash = s_Permutation[s_Permutation[s_Permutation[xIntegralPart] + yIntegralPart + 1] + zIntegralPart + 1];
+    int baaHash = s_Permutation[s_Permutation[s_Permutation[xIntegralPart + 1] + yIntegralPart]+ zIntegralPart];
+    int bbaHash = s_Permutation[s_Permutation[s_Permutation[xIntegralPart + 1] + yIntegralPart + 1] + zIntegralPart];
+    int babHash = s_Permutation[s_Permutation[s_Permutation[xIntegralPart + 1] + yIntegralPart] + zIntegralPart + 1];
+    int bbbHash = s_Permutation[s_Permutation[s_Permutation[xIntegralPart + 1] + yIntegralPart + 1] + zIntegralPart + 1];
 
     double aaaInfluence = Gradient(aaaHash, xFractionalPart, yFractionalPart, zFractionalPart);
 	double abaInfluence = Gradient(abaHash, xFractionalPart, yFractionalPart - 1, zFractionalPart);
@@ -127,7 +100,46 @@ static double Perlin(double x, double y, double z) {
 	return LinearInterpolation(zFadedFractionalPart, xyaInterpolation, xybInterpolation);;
 }
 
+static void GenerateRandomPermutation(void) {
+	srand(time(NULL));
+
+	int numbersLeft[256];
+	int numbersLeftCount = 256;
+
+	for (int i = 0; i < 256; i++) {
+		numbersLeft[i] = i;
+	}
+
+	int permutationCount = 0;
+
+	int drawnNumberIndex;
+	int drawnNumber;
+
+	while (numbersLeftCount > 0) {
+		// Draws a random number in `numbersLeft`
+		drawnNumberIndex = rand() % numbersLeftCount;
+		drawnNumber = numbersLeft[drawnNumberIndex];
+
+		// Adds `drawnNumber` to the `s_Permutation` array.
+		s_Permutation[permutationCount] = drawnNumber;
+		permutationCount++;
+
+		// Removes `drawnNumber` from `numbersLeft`.
+		numbersLeftCount--;
+
+		for (int i = drawnNumberIndex; i < numbersLeftCount; i++) {
+			numbersLeft[i] = numbersLeft[i + 1];
+		}
+	}
+
+	// Repeating the permutation for easier calculations
+	for (int i = 0; i < 256; i++) {
+		s_Permutation[i + 256] = s_Permutation[i];
+	}
+}
+
 static bool BrushPerlin_TryParseArguments(const cc_string* args, int argsCount) {
+	GenerateRandomPermutation();
 	return true;
 }
 
