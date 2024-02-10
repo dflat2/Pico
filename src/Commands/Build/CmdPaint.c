0276f2390934696e6facf2912287dc29b5fc7423
dflat2 <116065652+dflat2@users.noreply.github.com>
2024-02-10 11:38:40 +0100

    feat: make /paint static by default

--- src/Commands/Build/CmdPaint.c
+++ src/Commands/Build/CmdPaint.c
@@ -8,8 +8,6 @@
 #include "Message.h"
 #include "Parse.h"
 
-static bool s_Repeat = false;
-
 static void Paint_Command(const cc_string* args, int argsCount);
 static void PaintSelectionHandler(IVec3* marks, int count);
 
@@ -18,8 +16,8 @@ struct ChatCommand PaintCommand = {
     Paint_Command,
     COMMAND_FLAG_SINGLEPLAYER_ONLY,
     {
-        "&b/Paint [block/brush] +",
-        "Paints a block.",
+        "&b/Paint [block/brush]",
+        "Paints blocks.",
         NULL,
         NULL,
         NULL
@@ -28,8 +26,6 @@ struct ChatCommand PaintCommand = {
 };
 
 static void Paint_Command(const cc_string* args, int argsCount) {
-    s_Repeat = Parse_LastArgumentIsRepeat(args, &argsCount);
-
     if (argsCount >= 1) {
         if (!Parse_TryParseBlockOrBrush(&args[0], argsCount)) {
             return;
@@ -38,11 +34,7 @@ static void Paint_Command(const cc_string* args, int argsCount) {
         Brush_LoadInventory();
     }
 
-    if (s_Repeat) {
-        Message_Player("Now repeating &bPaint&f.");
-    }
-
-    Message_Player("&fPlace or break a block to paint.");
+    Message_Player("Place or break blocks to paint. Stop with &b/Abort&f.");
     MarkSelection_Make(&PaintSelectionHandler, 1, "Paint");
 }
 
@@ -53,12 +45,7 @@ static void PaintSelectionHandler(IVec3* marks, int count) {
 
     Draw_Start("Paint");
     Draw_Brush(marks[0].X, marks[0].Y, marks[0].Z);
-    int blocksAffected = Draw_End();
+    Draw_End();
 
-    if (s_Repeat) {
-        MarkSelection_Make(&PaintSelectionHandler, 1, "Paint");
-        return;
-    }
-
-    Message_BlocksAffected(blocksAffected);
+    MarkSelection_Make(&PaintSelectionHandler, 1, "Paint");
 }
